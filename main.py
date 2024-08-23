bbe5fe69a5e8bed4e8319646b117ad6b5bed1cfc
lauhkz <lautaro_RL@outlook.com>
2024-08-23 11:45:59 -0300

    reformated a bit, and implemented the count words feature

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 def main():
+    text = read_book()
+    count_words(text)
+    #print(text)
+
+def read_book():
     with open("books/frankenstein.txt") as f:
-        book_contents = f.read()
+        return f.read()
 
-        print(book_contents)
-
+def count_words(text):
+    counter = 0
+    words = text.split()
+    for word in words:
+        counter += 1
+    return counter
 
 main()
